facd3367dc62585be8421cb926ed65a75b8efb6d
jasanishubh123 <jasanishubham@gmail.com>
2020-07-13 16:49:52 +0530

    Updates

---
+++ precache-manifest.ddc72c4506758ecfdc31d20a43ec018f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "110ed3fb0cb62b42514c33b88f12999e",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "39843695e57fbfefa3b5",
+    "url": "/MyPortfolio/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "227730a96fb1a8c408b8",
+    "url": "/MyPortfolio/static/css/main.fc957020.chunk.css"
+  },
+  {
+    "revision": "39843695e57fbfefa3b5",
+    "url": "/MyPortfolio/static/js/2.8f696895.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyPortfolio/static/js/2.8f696895.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "227730a96fb1a8c408b8",
+    "url": "/MyPortfolio/static/js/main.71751dd7.chunk.js"
+  },
+  {
+    "revision": "93c65b4db3af1fa805d2",
+    "url": "/MyPortfolio/static/js/runtime-main.bd9119cd.js"
+  },
+  {
+    "revision": "f8e7f176839b1ad3ffe837b07a557846",
+    "url": "/MyPortfolio/static/media/Shubham_Resume.f8e7f176.PDF"
+  }
+]);
